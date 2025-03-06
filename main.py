3fcd6db288d128fd02aeccf4bee220d8142efe68
Shubhankar721 <t220128@famt.ac.in>
2025-03-07 00:01:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -27,10 +27,21 @@ if uploaded_file is not None:
     x_columns = st.selectbox("Select x-axis column",columns)
     y_columns = st.selectbox("Select y-axis column",columns)
 
-    if st.button("Generate Plot"):
-        if x_columns in filtered_df.columns and y_columns in filtered_df.columns:
-            st.line_chart(filtered_df.set_index(x_columns)[y_columns])
-        else:
-            st.error(f"Error: The selected column '{y_columns}' does not exist in the filtered data.")
+    if filtered_df.empty:
+        st.warning("No data matches the selected filter.")
+    else:
+
+        st.subheader("Plot Data")
+        x_columns = st.selectbox("Select x-axis column", columns)
+        y_columns = st.selectbox("Select y-axis column", columns)
+
+        if st.button("Generate Plot"):
+            if x_columns in filtered_df.columns and y_columns in filtered_df.columns:
+                if pd.api.types.is_numeric_dtype(filtered_df[y_columns]):
+                    st.line_chart(filtered_df.set_index(x_columns)[y_columns])
+                else:
+                    st.error(f"Error: The selected column '{y_columns}' must contain numeric data.")
+            else:
+                st.error(f"Error: The selected column '{y_columns}' does not exist in the filtered data.")
 else:
-    st.write("Wainting for fileto upload...!") 
+    st.write("Wainting for file to upload...!") 
