e439b200a2cb68034a543844e9af5d2845f00194
Shubhankar721 <t220128@famt.ac.in>
2025-03-07 00:14:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ if uploaded_file is not None:
         st.subheader("Plot Data")
         x_columns = st.selectbox("Select x-axis column", columns)
         y_columns = st.selectbox("Select y-axis column", columns)
-        if (x_columns == y_columns):
+        if (x_columns != y_columns):
             if st.button("Generate Plot"):
                 if x_columns in filtered_df.columns and y_columns in filtered_df.columns :
                     if pd.api.types.is_numeric_dtype(filtered_df[y_columns]):
